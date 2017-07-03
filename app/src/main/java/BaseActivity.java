ab944b060d5e7d5a41e53f7ce25663c52221ad90
jf <190310578@qq.com>
2017-07-03 15:25:46 +0800

    头像

---
+++ app/src/main/java/BaseActivity.java
@@ -0,0 +1,17 @@
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.zhy.autolayout.AutoLayoutActivity;
+
+/**
+ * Created by Administrator on 2017/6/29.
+ */
+
+public class BaseActivity extends AutoLayoutActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+}
